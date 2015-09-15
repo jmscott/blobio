55672565c501ab8d2caada415e487478482e436f
jmscott <jmscott@6b17d688-bdc0-4895-adc2-ad75aa541eef>
2015-09-15 23:01:06 +0000

    merge with git

---
+++ blobio.mk
@@ -0,0 +1,18 @@
+#
+#  Synopsis:
+#	Static make rules for blobio.
+#  Blame:
+#  	jmscott@setspace.com
+#  	setspace@gmail.com
+#
+
+#  Most extreme compilation flags
+
+CFLAGS=-Wall -Wextra -Werror -DSHA_FS_MODULE -DSK_FS_MODULE
+
+#  Setting SHELL forces gmake to disable optimizing of single line productions,
+#  forcing make to honor $PATH
+
+SHELL=/bin/bash
+
+UNAME=$(shell uname)
