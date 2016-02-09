762e40aedac748bcd8e303c6db5c8f622b860b1d
John the Scott <jmscott@setspace.com>
2016-02-08 20:54:25 -0600

    added get/verbs

--- cli/blobio/rofs.c
+++ cli/blobio/rofs.c
@@ -9,16 +9,19 @@
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
+#include <fcntl.h>
 
 #include "blobio.h"
 
 #ifdef COMPILE_TRACE
 
 #define _TRACE(msg)		if (tracing) _trace(msg)
+#define _TRACE2(msg1,msg2)	if (tracing) _trace2(msg1,msg2)
 
 #else
 
 #define _TRACE(msg)
+#define _TRACE2(msg1,msg2)
 
 #endif
 
@@ -31,12 +34,21 @@ extern int	output_fd;
 extern char	*input_path;
 extern int	input_fd;
 
+//  Note: rename as fs_path
+static char	fs_path[PATH_MAX + 1];
+
 struct service rofs_service;
 
 static void
 _trace(char *msg)
 {
 	trace2("rofs", msg);
+}
+
+static void
+_trace2(char *msg1, char *msg2)
+{
+	trace3("rofs", msg1, msg2);
 }
 
 /*
@@ -84,41 +96,56 @@ static char *
 rofs_open()
 {
 	struct stat st;
-	char data_path[PATH_MAX + 1];
 
 	_TRACE("request to open()");
 
-	//  verify root blob directory exists and is statable.
+	_TRACE2("blob root directory", end_point);
 
-	if (stat(end_point, &st)) {
+	//  verify permissons
+
+	if (access(end_point, X_OK)) {
 		if (errno == ENOENT)
-			return "root blob directory does not exist";
+			return "blob root directory does not exist";
 		if (errno == EPERM)
-			return "no permission to access root blob directory";
+			return "no permission to search blob root directory";
 		return strerror(errno);
 	}
+			
+	//  verify the end point is, in fact, a directory
 
-	if ((st.st_mode & S_IFDIR) == 0)
+	if (stat(end_point, &st))
+		return strerror(errno);
+	if (!S_ISDIR(st.st_mode))
 		return "root blob directory is not a directory";
+
+	//  assemble the path to the data directory
+
+	fs_path[0] = 0;
+	bufcat(fs_path, sizeof fs_path, end_point);
+	bufcat(fs_path, sizeof fs_path, "/data/");
+	bufcat(fs_path, sizeof fs_path, algorithm);
+	bufcat(fs_path, sizeof fs_path, "_fs");
 
 	//  verify existence and permission of path to data directory.
 	//  path looks like <root_dir>/data/<algorithm>_fs
 
-	data_path[0] = 0;
-	bufcat(data_path, sizeof data_path, end_point);
-	bufcat(data_path, sizeof data_path, "/data/");
-	bufcat(data_path, sizeof data_path, algorithm);
-	bufcat(data_path, sizeof data_path, "_fs");
+	_TRACE2("blob data directory", fs_path);
 
-	if (stat(data_path, &st)) {
+	//  verify permissons
+
+	if (access(fs_path, X_OK)) {
 		if (errno == ENOENT)
 			return "blob data directory does not exist";
 		if (errno == EPERM)
-			return "no permission to blob data directory";
+			return "no permission to search blob data directory";
 		return strerror(errno);
 	}
+			
+	//  verify the data directory is, in fact, a directory
 
-	if ((st.st_mode & S_IFDIR) == 0)
+	if (stat(fs_path, &st))
+		return strerror(errno);
+	if (!S_ISDIR(st.st_mode))
 		return "blob data directory is not a directory";
 
 	_TRACE("open() done");
@@ -149,29 +176,125 @@ rofs_open_output()
 static char *
 rofs_close()
 {
+	_TRACE("request to close()");
+
+	_TRACE("close() done");
+
 	return (char *)0;
 }
 
 /*
- *  Link a
+ *  Get a blob.
+ *
+ *  If output path exists then the output path to the blob path;
+ *  otherwise copy the file to the output
  */
 static char *
 rofs_get(int *ok_no)
 {
-	(void)ok_no;
+	char *err;
+	int len, blob_fd;
+	unsigned char buf[PIPE_MAX];
+	ssize_t nread;
+
 	_TRACE("request to get()");
+
+	//  build path to source blob file
+
+	bufcat(fs_path, sizeof fs_path, "/");
+	len = strlen(fs_path);
+	err = rofs_service.digest->fs_path(
+				fs_path + len,
+				PATH_MAX - len
+	);
+	if (err)
+		return err;
+
+	_TRACE2("blob file path", fs_path);
+
+	//  link output path to source blob file
+	
+	if (output_path) {
+		_TRACE("hard link to source blob file");
+
+		if (link(fs_path, output_path)) {
+			if (errno == ENOENT) {
+				*ok_no = 1;
+				return (char *)0;
+			}
+			return strerror(errno);
+		}
+		*ok_no = 0;
+		return (char *)0;
+	}
+
+	//  no output path so copy blob to output file
+	
+	_TRACE("copying blob to output stream");
+
+	//  Note:  think about linux splice()
+
+	if ((blob_fd = uni_open(fs_path, O_RDONLY)) < 0)
+		return strerror(errno);
+
+	//  Note: stat size?
+
+	while ((nread = uni_read(blob_fd, buf, sizeof buf)) > 0)
+		if (uni_write_buf(output_fd, buf, nread))
+			return strerror(errno);
+	if (nread < 0)
+		return strerror(errno);
+	if (uni_close(blob_fd))
+		return strerror(blob_fd);
+
 	_TRACE("get() done");
 	return (char *)0;
 }
 
+/*
+ *  Eat a blob file by verifying existence and readability.
+ */
 static char *
 rofs_eat(int *ok_no)
 {
-	(void)ok_no;
+	char *err;
+	int len;
 
 	_TRACE("request to eat()");
 
-	_TRACE("eat() done");
+	bufcat(fs_path, sizeof fs_path, "/");
+	len = strlen(fs_path);
+	err = rofs_service.digest->fs_path(
+				fs_path + len,
+				PATH_MAX - len
+	);
+	if (err)
+		return err;
+
+	_TRACE2("blob file path", fs_path);
+
+	//  insure we can read the file.
+
+	if (access(fs_path, R_OK)) {
+		if (errno == ENOENT) {
+			_TRACE("blob does not exists");
+			*ok_no = 1;
+			return (char *)0;
+		}
+		return strerror(errno);
+	}
+
+	*ok_no = 0;
+
+#ifdef COMPILE_TRACE
+	if (tracing) {
+		TRACE("blob is readable");
+		TRACE("eat() done");
+	}
+#endif
+
+	return (char *)0;
+
 
 	return (char *)0;
 }
