42d7bd48243dd9f7646c89d476cce8601b08d890
John the Scott <jmscott@setspace.com>
2016-02-11 09:05:07 -0600

    zapped compiler option -std=c11

--- blobio.mk
+++ blobio.mk
@@ -8,7 +8,7 @@
 
 #  Most extreme compilation flags
 
-CFLAGS=-std=c11 -Wall -Wextra -Werror -DSHA_FS_MODULE
+CFLAGS=-Wall -Wextra -Werror -DSHA_FS_MODULE
 
 #  Setting SHELL forces gmake to disable optimizing of single line productions,
 #  forcing make to honor $PATH
