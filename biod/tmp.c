1ca96429ff6864bf8d41febe71854e7100506858
John the Scott <jmscott@setspace.com>
2017-03-07 14:30:29 -0600

    tmp.c: more error checking on value of BLOBIO_TMPDIRS

--- biod/tmp.c
+++ biod/tmp.c
@@ -15,14 +15,17 @@
  *	digest onto a temp directory path for use with the blob.  The syntax
  *	is
  *
- *		BLOBIO_TMPDIRS=<prefix>\t<path>\t<prefix>\t<path>
+ *		BLOBIO_TMPDIRS=<algo>:<prefix>\t<path>\t<algo>:<prefix>\t<path>
+ *		BLOBIO_TMPDIRS=sha:a\t
  *	
  *	The <path> must start with / .  If no match occurs, then $BLOBIO_TMPDIR
  *	will be tried, followed by $BLOBIO_ROOT/tmp.
  *  Note:
  *	This whole code is kind of a hack for casual users.
  *
- *	Need to investigate/document if LLVM can extend inodes dynamically.
+ *	Only ascii file paths are understood, which is wrong.
+ *
+ *	Need to investigate/document if LVM can extend inodes dynamically.
  */
 #include <stdlib.h>
 #include "biod.h"
@@ -30,11 +33,23 @@
 extern char *BLOBIO_ROOT;
 
 static char *BLOBIO_TMPDIR = (char *)0;
-static char *BLOBIO_TMPDIRS = (char *)0;
+
+struct tmp_path
+{
+	char	*algorithm;
+	char	*digest_prefix;
+	char	*path;
+};
 
 void
 tmp_open()
 {
+	char *BLOBIO_TMPDIRS;
+	char td[4096];
+
+	int tab_count;
+	char *p;
+
 	BLOBIO_TMPDIR = getenv("BLOBIO_TMPDIR");
 	if (BLOBIO_TMPDIR == NULL)
 		info("env BLOBIO_TMPDIR: not defined");
@@ -51,5 +66,38 @@ tmp_open()
 
 	if (strlen(BLOBIO_TMPDIRS) >= 4096)
 		panic("env variable BLOBIO_TMPDIRS >= 4096 bytes");
+	strcpy(td, BLOBIO_TMPDIRS);
+
+	//  verify an odd number of tab chars and replace \t char with null
+
+	p = td;
+	for (tab_count = 0;  p != NULL;  (p = strchr(p, '\t'))) {
+		tab_count++;
+		*p = 0;
+	}
+	if (tab_count == 0)
+		panic2("no tabs in BLOBIO_TMPDIRS", BLOBIO_TMPDIRS);
+	if (tab_count % 2 == 0)
+		panic2("even number of tab chars in BLOBIO_TMPDIRS",
+							BLOBIO_TMPDIRS);
+
+	//  verify each algorithm exists
+
+	p = td;
+	while (*p) {
+		char *q, algo[9], digest[129];
+
+		q = strchr(p, ':');
+		if (q == NULL)
+			panic3("BLOBIO_TMPDIRS", "no colon after algorithm",td);
+		if (q - p > 8)
+			panic3("BLOBIO_TMPDIRS", "algorithm > 8 bytes", td);
+		memcpy(algo, p, q - p);
+		algo[q - p] = 0;
+		if (module_get(algo) == NULL)
+			panic3("BLOBIO_TMPDIRS", "unknown digest algorithm", 
+							algo);
+	}
+
 	info2("BLOBIO_TMPDIRS", BLOBIO_TMPDIRS);
 }
