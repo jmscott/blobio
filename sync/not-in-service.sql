254f5d45f05307cb68f11d5ccae562c03af6fdd7
John the Scott <jmscott@setspace.com>
2018-06-13 14:12:02 -0500

    created not-in-service.sql

--- sync/not-in-service.sql
+++ sync/not-in-service.sql
@@ -1,10 +1,17 @@
+/*
+ *  Synopsis:
+ *	Find udigs from stdin that are not in service table.
+ *  Usage:
+ *	sync-brr-chain						|
+ *		grep '^[0-9a-z][0-9a-z]*'			|
+ *		psql -f not-in-service.sql >not-in-service.udig
+ *	wc -l chain-not-in-service.udig
+ */
 \set ON_ERROR_STOP 1
 \timing 1
 
-/*
- *  Most recently seen successfull take FROM this service.
- *  The connection chat history was ok,ok,ok, hence ok3.
- */
+SET search_path TO blobio,public;
+
 CREATE TEMP TABLE chain
 (
 	blob			udig
@@ -14,12 +21,19 @@ CREATE INDEX chain_hash ON
 	chain USING hash(blob)
 ;
 
-\copy chain from chain.udig
+\copy chain from pstdin
 ANALYZE chain;
 
 \pset tuples_only
 \pset format unaligned
-\o chain-not-in-service.udig
+SELECT
+	count(*) AS chain_count
+  FROM
+  	chain
+\gset
+\echo
+\echo Chain count is :chain_count
+
 WITH out_of_service AS (
   SELECT
 	c.blob
