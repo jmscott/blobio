ad18bd472e6e31152ea44bc407377b0841dd6831
John the Scott <jmscott@setspace.com>
2018-12-31 19:52:34 -0700

    added tip_compare() report

--- sync/jaquard.go
+++ sync/jaquard.go
@@ -37,8 +37,19 @@ type PGDatabase struct {
 	db			*sql.DB
 
 	SystemIdentifier	string		`json:"system_identifier"`
-	TipUDIG			ServiceUDIG	`json:"tip_udig"`
+	TipUDig			ServiceUDIG	`json:"tip_udig"`
 	Stats			sql.DBStats	`json:"stats"`
+}
+
+type TipQuery struct {
+
+	SourceDB	string	`json:"source_db"`
+	source		*PGDatabase
+
+	TargetDB	string	`json:"target_db"`
+	target		*PGDatabase
+
+	SourceInTarget	bool	`json:"source_in_target"`
 }
 
 type Config struct {
@@ -51,11 +62,14 @@ type Config struct {
 
 type Answer struct {
 
+	TipNotExistsCount	int		`json:"tip_not_exists_count"`
+	TipExistsCount		int		`json:"tip_exists_count"`
 	StartTime		time.Time	`json:"start_time"`
 	WallDuration		time.Duration	`json:"wall_duration"`
 	WallDurationString	string		`json:"wall_duration_string"`
 
 	Databases	map[string]*PGDatabase	`json:"databases"`
+	TipQueries		[]TipQuery	`json:"tip_queries"`
 }
 
 const usage = "usage: jaquard <config-file-path>\n"
@@ -113,11 +127,9 @@ func (conf *Config) open() {
 		go pg.open(done)
 	}
 
-	cnt := len(conf.Databases)
-
 	//  insure system ids for databases are distinct
-	sid := make(map[string]bool, cnt) 
-	for ;  cnt > 0;  cnt-- {
+	sid := make(map[string]bool, len(conf.Databases)) 
+	for cnt := len(conf.Databases);  cnt > 0;  cnt-- {
 
 		//  Note:  do we need a timeout?
 		pg := <- done
@@ -155,8 +167,7 @@ func (conf *Config) close() {
 	}
 }
 
-func (pg *PGDatabase) jaquard(done chan *PGDatabase) {
-
+func (pg *PGDatabase) select_tip(done chan bool) {
 
 	sql := `
 SELECT
@@ -171,23 +182,84 @@ SELECT
 `
 	err := pg.db.
 		QueryRow(sql).
-		Scan(&pg.TipUDIG.Blob, &pg.TipUDIG.DiscoverTime)
+		Scan(&pg.TipUDig.Blob, &pg.TipUDig.DiscoverTime)
 	if err != nil {
-		pg.die("QueryRow(jaquard) failed: %s", err)
+		pg.die("QueryRow(tip) failed: %s", err)
 	}
-	done <- pg
+	done <- true
 }
 
-func (conf *Config) jaquard() {
+func (conf *Config) select_tip() {
 
-	done := make(chan *PGDatabase)
+	done := make(chan bool)
 	for _, pg := range conf.Databases {
-		go pg.jaquard(done)
+		go pg.select_tip(done)
+	}
+	for cnt := len(conf.Databases);  cnt > 0;  cnt-- {
+		<- done
+	}
+}
+
+func (tq *TipQuery) exists(done chan bool) {
+
+	sql := `
+SELECT EXISTS (
+  SELECT
+  	blob
+    FROM
+    	blobio.service
+    WHERE
+    	blob = $1
+)`
+	err := tq.target.db.QueryRow(
+			sql,
+			tq.source.TipUDig.Blob,
+	).Scan(&tq.SourceInTarget)
+	if err != nil {
+		die("tip query(" +
+			tq.SourceDB +
+			", " + tq.TargetDB + "): sql.QueryRow() failed: %s",
+			err,
+		)
+		die("WTF")
+	}
+	done <- tq.SourceInTarget
+}
+
+func (answer *Answer) compare_tip(dbs map[string]*PGDatabase) {
+
+	done := make(chan bool)
+
+	//  check that tip for each database exists in other peer databases
+
+
+	cnt := len(dbs) * (len(dbs) - 1)
+	answer.TipQueries = make([]TipQuery, cnt)
+	i := 0
+
+	for src, src_pg := range dbs {
+		for tgt, tgt_pg := range dbs {
+			if src == tgt {
+				continue
+			}
+
+			tq := &answer.TipQueries[i]
+			tq.SourceDB = src
+			tq.source = src_pg
+			tq.TargetDB = tgt
+			tq.target = tgt_pg
+			go tq.exists(done)
+			i++
+		}
 	}
 
-	cnt := len(conf.Databases)
-	for ;  cnt > 0;  cnt-- {
-		<- done
+	//  wait for tip queries to finish
+	for i = 0;  i < cnt;  i++ {
+		if <- done {
+			answer.TipExistsCount++
+		} else {
+			answer.TipNotExistsCount++
+		}
 	}
 }
 
@@ -209,6 +281,7 @@ func (pg *PGDatabase) open(done chan *PGDatabase) {
 		pg.die("sql.Open() failed: %s", err)
 	}
 
+	//  insure each database has unque system identifier
 	err = pg.db.QueryRow(
 		`SELECT system_identifier FROM pg_control_system();`,
 	).Scan(&pg.SystemIdentifier)
@@ -218,7 +291,6 @@ func (pg *PGDatabase) open(done chan *PGDatabase) {
 	if len(pg.SystemIdentifier) == 0 {
 		pg.die("empty system identifier")
 	}
-
 	done <- pg
 }
 
@@ -259,7 +331,8 @@ func main() {
 
 	conf.frisk()
 	conf.open()
-	conf.jaquard()
+	conf.select_tip()
+	answer.compare_tip(conf.Databases)
 	conf.close()
 	answer.Databases = conf.Databases
 
