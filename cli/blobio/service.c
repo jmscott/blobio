40df1c05df6380c871cb9130274e1d4c2754ea92
John the Scott <jmscott@setspace.com>
2019-06-10 17:09:55 -0500

    zapped service rofs

--- cli/blobio/service.c
+++ cli/blobio/service.c
@@ -6,14 +6,12 @@
 
 extern struct service	bio4_service;
 extern struct service	cache4_service;
-extern struct service	rofs_service;
 extern struct service	fs_service;
 
 struct service *services[] =
 {
 	&bio4_service,
 	&cache4_service,
-	&rofs_service,
 	&fs_service,
 	(struct service *)0
 };
