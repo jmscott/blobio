e34af8d595ad22b409cbe742a89e86a0912c63ea
John the Scott <jmscott@setspace.com>
2019-06-13 12:51:10 -0500

    forced gcc to standard iso99

--- blobio.mk
+++ blobio.mk
@@ -2,17 +2,20 @@
 #  Synopsis:
 #	Static make rules for blobio.
 #
+UNAME=$(shell uname)
 
 #  Most extreme compilation flags
 
 CFLAGS?=-Wall -Wextra -Werror -DSHA_FS_MODULE -DBC160_FS_MODULE
 
+ifeq "$(UNAME)" "Linux"
+	CFLAGS+=-std=gnu99
+endif
+
 #  Setting SHELL forces gmake to disable optimizing of single line productions,
 #  forcing make to honor $PATH
 
 SHELL?=/bin/bash
-
-UNAME=$(shell uname)
 
 DIST_USER?=$(USER)
 ifeq "$(UNAME)" "Darwin"
