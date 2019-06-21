4681a9089d533c442ff7b417e3936f54a2765da9
John the Scott <jmscott@setspace.com>
2019-06-20 19:15:02 -0500

    checkpoint

--- cli/blobio/digest.c
+++ cli/blobio/digest.c
@@ -24,3 +24,43 @@ struct digest *digests[] =
 
 	(struct digest *)0
 };
+
+#ifdef NO_COMPILE
+/*
+ *  Copy input to output up to end of file,
+ *  croaking if the input does not match the
+ */
+char *
+digest_copy_eof(int in_fd, int out_fd)
+{
+	int nread;
+	char buf[PIPE_MAX];
+	char *status;
+
+	if ((status = digest->init()))
+		return status;
+	while ((nread = uni_read(in_fd, buf, sizeof buf)) > 0) {
+		status = digest->get_update();
+		if (strlen(status) > 1)
+			return status;
+		if (status[0] == '0')
+			break;
+	}
+	if
+	if (status[0] != '0')
+		return "digest_copy_eof: end of stream does not match digest";
+	|| status[0] != '0')	//  no one char errors
+		return status;
+
+	if ((err = digest->sha_get_update()))
+		return err;
+
+	while ((nread = uni_read(input_fd, buf, sizeof buf)) > 0)
+		if (!SHA1_Update(&sha_ctx, buf, nread))
+			return "SHA1_Update(chunk) failed";
+	if (nread < 0)
+		return strerror(errno);
+	if (!SHA1_Final(bin_digest, &sha_ctx))
+		return "SHA1_Final() failed";
+}
+#endif
