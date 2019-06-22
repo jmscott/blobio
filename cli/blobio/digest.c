b1df4be8dc46cc6ef52847ff699118ba2cc9614c
John the Scott <jmscott@setspace.com>
2019-06-22 13:52:45 -0500

    added digest_copy() for eventual use with sha/bc160

--- cli/blobio/digest.c
+++ cli/blobio/digest.c
@@ -2,7 +2,11 @@
  *  Synopsis:
  *	Hash Digest Modules
  */
+#include <string.h>
 #include "blobio.h"
+
+extern struct digest	*digest_module;
+extern int		errno;
 
 #ifdef SHA_FS_MODULE
 extern struct digest	sha_digest;
@@ -25,42 +29,50 @@ struct digest *digests[] =
 	(struct digest *)0
 };
 
-#ifdef NO_COMPILE
 /*
- *  Copy input to output up to end of file,
- *  croaking if the input does not match the
+ *  Digest the input stream explicity up to end of stream, incrementally
+ *  writing to output stream.
+ *
+ *	return == (char *)0	successfully copied data matching digest
+ *	strlen(return) > 1	error message
+ *
+ *  Note:
+ *	Timeout not supported.
  */
 char *
-digest_copy_eof(int in_fd, int out_fd)
+digest_copy(int in_fd, int out_fd)
 {
 	int nread;
-	char buf[PIPE_MAX];
-	char *status;
+	unsigned char buf[PIPE_MAX];
+	char *status = "";
 
-	if ((status = digest->init()))
+	if ((status = digest_module->init()))
 		return status;
 	while ((nread = uni_read(in_fd, buf, sizeof buf)) > 0) {
-		status = digest->get_update();
+		status = digest_module->get_update(buf, sizeof buf);
 		if (strlen(status) > 1)
 			return status;
+		if (uni_write_buf(out_fd, buf, nread))
+			return strerror(errno);
 		if (status[0] == '0')
 			break;
 	}
-	if
-	if (status[0] != '0')
-		return "digest_copy_eof: end of stream does not match digest";
-	|| status[0] != '0')	//  no one char errors
-		return status;
-
-	if ((err = digest->sha_get_update()))
-		return err;
-
-	while ((nread = uni_read(input_fd, buf, sizeof buf)) > 0)
-		if (!SHA1_Update(&sha_ctx, buf, nread))
-			return "SHA1_Update(chunk) failed";
+	if (nread > 0)				//  status must == "0"
+		return (char *)0;
 	if (nread < 0)
 		return strerror(errno);
-	if (!SHA1_Final(bin_digest, &sha_ctx))
-		return "SHA1_Final() failed";
+
+	//  read returned 0
+
+	if (status[0] == '1')
+		return "unexpected end of file before digest seen";
+
+	if (status[0])
+		return (char *)0;
+
+	//  first read returned 0 so digest must be empty to be correct
+
+	if (digest_module->empty())
+		return (char *)0;
+	return "empty stream for full digest";
 }
-#endif
