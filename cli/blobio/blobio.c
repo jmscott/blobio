370f2e55442d5b95d5d7929dcc6f06979d90e51b
John the Scott <jmscott@setspace.com>
2019-09-24 21:24:58 -0500

    improved note on syscall to optimize i/o on linux

--- cli/blobio/blobio.c
+++ cli/blobio/blobio.c
@@ -16,8 +16,8 @@
  *	--output-path <path/to/file>
  *	--help
  *  Note:
- *	File service fs:/usr/local/blob need to implement system call
- *	sendfile() or copy_file_range() on linux.
+ *	Investigate linux system calls splice(), sendfile() and
+ *	copy_file_range().
  *
  *	Timeout needs to be explicit status, so network flaps can be
  *	diagnosed.
