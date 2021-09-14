f4250af3e70ae4ea3229585adca565ccdf238902
John the Scott <jmscott@setspace.com>
2021-09-14 17:15:15 -0500

    working version of stage-fdr2rrd.sql

---
+++ flowd/stage-fdr2rrd.sql
@@ -0,0 +1,54 @@
+/*
+ *  Synopsis:
+ *	Generate Round Robin Database update samples on standard output.
+ *  Description:
+ *	Looking back two minutes, write a minute stepped set of tuples
+ *	suitable for rrdupdate. A "zero" fdr record means both ok_count
+ *	and fault_count are zero.
+ *
+ *		epoch:			#  rounded to a minute
+ *		fdr_count:		#  number of flow desc records/min
+ *		blob_count:		#  distinct number of blobs/min
+ *		blob_size:		#  sum of byte count for fdr blobs
+ *		ok_count:		#  total number of ok countd/min
+ *		fault_count:		#  total number of faults countd/min
+ *		wall_duration_sec:	#  sec rounded sum of wall durations
+ *		zero_fdr_count:		#  number of zero flow records
+ *		zero_blob_count:	#  distinct number of blobs zero fdr
+ */
+SET search_path TO blobio,public;
+
+\x
+SELECT
+	extract('epoch' from
+		date_trunc('minute', st.start_time + st.wall_duration))
+		AS start_minute,
+	count(*)
+		AS fdr_count,
+	sum(bs.byte_count)
+		FILTER(WHERE st.ok_count > 0 OR st.fault_count > 0)
+		AS fdr_blob_size,
+	count(DISTINCT st.blob)
+		AS blob_count,
+	sum(st.ok_count)
+		AS ok_count,
+	sum(st.fault_count)
+		AS fault_count,
+	round(extract('second' from sum(st.wall_duration))::numeric, 0)
+		AS wall_duration_sec,
+	count(*)
+		FILTER(WHERE st.ok_count = 0 AND st.fault_count = 0)
+	  	AS zero_fdr_count,
+	count(DISTINCT st.blob)
+		FILTER(WHERE st.ok_count = 0 AND st.fault_count = 0)
+		AS zero_blob_count
+  FROM
+	rrd_stage_fdr st
+	  LEFT OUTER JOIN brr_blob_size bs ON (
+	  	bs.blob = st.blob
+	  )
+  GROUP BY
+  	start_minute
+  ORDER BY
+  	start_minute ASC
+;
