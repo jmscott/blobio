11a237213dc6a66930bafc9d710931f7c2caee33
John the Scott <jmscott@setspace.com>
2021-11-23 11:32:50 -0600

    added defer close(socket) to probe_ip_port()

--- www/noc/service/service.d/common.pl
+++ www/noc/service/service.d/common.pl
@@ -24,50 +24,71 @@ sub probe_ip_port
         }
 
         # Create socket.
-        socket(my $connection, PF_INET, SOCK_STREAM, getprotobyname('tcp'))
+        socket(my $con, PF_INET, SOCK_STREAM, getprotobyname('tcp'))
 						or return 0;
+	#  run probe code in eval, so socket is properly closed.
+	#  not properly closing the socket seems to confuse xinetd
+	#  on mac big sur.  details.
 
-        # autoflush
-        $_ = select($connection); $| = 1; select $_;
+	my $is_open = eval
+	{
+		# autoflush
+		$_ = select($con);  $| = 1;  select $_;
 
-        # Set FD_CLOEXEC.
-        $_ = fcntl($connection, F_GETFL, 0) or return 0;
-        fcntl($connection, F_SETFL, $_ | FD_CLOEXEC) or return 0;
+		$_ = fcntl($con, F_GETFL, 0) or return 0;
+		fcntl($con, F_SETFL, $_ | FD_CLOEXEC) or return 0;
 
-        if($timeout) {
-                # Set O_NONBLOCK so we can time out connect().
-                $_ = fcntl($connection, F_GETFL, 0) or return 0;
-                fcntl($connection, F_SETFL, $_ | O_NONBLOCK) or return 0;
-        }
+		if ($timeout > 0) {
+			# Set O_NONBLOCK so we can time out connect().
+			$_ = fcntl($con, F_GETFL, 0) or return 0;
+			fcntl($con, F_SETFL, $_ | O_NONBLOCK) or return 0;
+		}
 
-        # in async mode, connect() always answers, even when no listener.  
-        connect($connection, pack_sockaddr_in($port, inet_aton($ip))) or
-		$!{EINPROGRESS} or return 0;
+		#  in async mode, connect() always answers,
+		#  even when no listener, so defer "Connection refused"
+		#  after connect().
 
-        # disable O_NONBLOCK.
-        $_ = fcntl($connection, F_GETFL, 0) or return 0;
-        fcntl($connection, F_SETFL, $_ & ~O_NONBLOCK) or return 0;
+		connect($con, pack_sockaddr_in($port, inet_aton($ip))) or
+					$!{EINPROGRESS} or return 0;
 
-        #  Use select() to poll for completion or error.
-	#  When connect succeeds we can write.
-        my $vec;
-        vec($vec, fileno($connection), 1) = 1;
-        select(undef, $vec, undef, $timeout);
-        return 0 unless(vec($vec, fileno($connection), 1));
+		# reset connectio to synchronous
 
-	#
-	#  Note:
-        #	we get a connection, even on error, so unpack structure to
-	#	determine error!  got to be a better method!
-	#
-        $! = unpack("L", getsockopt($connection, SOL_SOCKET, SO_ERROR));
-        return 0 if $!;
+		$_ = fcntl($con, F_GETFL, 0) or return 0;
+		fcntl($con, F_SETFL, $_ & ~O_NONBLOCK) or return 0;
 
-        setsockopt($connection, SOL_SOCKET, SO_SNDTIMEO, pack("L!L!",
-						$timeout, 0)) or return 0;
-        setsockopt($connection, SOL_SOCKET, SO_RCVTIMEO, pack("L!L!",
-						$timeout, 0)) or return 0;
-        return 1
+		#  Use select() to poll for completion or error.
+
+		my $vec;
+		vec($vec, fileno($con), 1) = 1;
+		select(undef, $vec, undef, $timeout);
+		return 0 unless(vec($vec, fileno($con), 1));
+
+		#
+		#  Note:
+		#	we get a connection, even on error, so see
+		#	if getsockopt correctly builds the socket
+		#	structute.  surely, a less hackish method
+		#	exists.
+		#
+		$! = unpack("L", getsockopt($con, SOL_SOCKET, SO_ERROR));
+		return 0 if $!;
+
+		setsockopt(
+			$con,
+			SOL_SOCKET,
+			SO_SNDTIMEO,
+			pack("L!L!", $timeout, 0)
+		) or return 0;
+		setsockopt(
+			$con,
+			SOL_SOCKET,
+			SO_RCVTIMEO,
+			pack("L!L!", $timeout, 0)
+		) or return 0;
+		return 1;
+	};
+	close($con);
+	return $is_open; 
 }
 
 1;
