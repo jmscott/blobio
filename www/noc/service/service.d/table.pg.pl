e1d1a950fce4aa266fa36b3c8c28a6d6029e76d6
John the Scott <jmscott@setspace.com>
2021-11-23 21:09:53 -0600

    stable checkpoint of pg stats <= 72 hrs

--- www/noc/service/service.d/table.pg.pl
+++ www/noc/service/service.d/table.pg.pl
@@ -83,14 +83,71 @@ $q = dbi_pg_select(
 	tag =>	'select-blobnoc-service-pg-stat',
 	argv =>	[
 			$PGDATABASE,
-			$PGDATABASE,
+			$$PGDATABASE,	# Note: zap me!
 		],
 	sql => q(
 SELECT
 	pg_size_pretty(pg_database_size($1))
 		AS database_size_english,
 	pg_database_size($2)
-		AS database_size_bytes
+		AS database_size_bytes,
+
+	--  previous second of blob traffic
+	count(*) FILTER(WHERE srv.discover_time >= now() + '-1 second')
+		AS blob_count_sec,
+	sum(sz.byte_count) FILTER(
+		WHERE srv.discover_time >= now() + '-1 second'
+	) AS blob_size_sec,
+	pg_size_pretty(sum(sz.byte_count) FILTER(
+		WHERE srv.discover_time >= now() + '-1 second'
+	)) AS blob_size_english_sec,
+
+	--  previous minute of blob traffic
+	count(*) FILTER(WHERE srv.discover_time >= now() + '-1 minute')
+		AS "blob_count_min",
+	sum(sz.byte_count) FILTER(
+		WHERE srv.discover_time >= now() + '-1 minute'
+	) AS blob_size_min,
+	pg_size_pretty(sum(sz.byte_count) FILTER(
+		WHERE srv.discover_time >= now() + '-1 minute'
+	)) AS blob_size_english_min,
+
+	--  previous hour of traffic
+	count(*) FILTER(WHERE srv.discover_time >= now() + '-1 hour')
+		AS "blob_count_hr",
+	sum(sz.byte_count) FILTER(
+		WHERE srv.discover_time >= now() + '-1 hour'
+	) AS blob_size_hr,
+	pg_size_pretty(sum(sz.byte_count) FILTER(
+		WHERE srv.discover_time >= now() + '-1 hour'
+	)) AS blob_size_english_hr,
+
+	--  previous 24 hours of traffic
+	count(*) FILTER(WHERE srv.discover_time >= now() + '-24 hour')
+		AS "blob_count_24hr",
+	sum(sz.byte_count) FILTER(
+		WHERE srv.discover_time >= now() + '-24 hours'
+	) AS blob_size_24hr,
+	pg_size_pretty(sum(sz.byte_count) FILTER(
+		WHERE srv.discover_time >= now() + '-24 hours'
+	)) AS blob_size_english_24hr,
+
+	--  previous 72 hours of traffic
+	count(*) FILTER(WHERE srv.discover_time >= now() + '-72 hours')
+		AS "blob_count_72hr",
+	sum(sz.byte_count) FILTER(
+		WHERE srv.discover_time >= now() + '-72 hours'
+	) AS blob_size_72hr,
+	pg_size_pretty(sum(sz.byte_count) FILTER(
+		WHERE srv.discover_time >= now() + '-72 hours'
+	)) AS blob_size_english_72hr
+  FROM
+  	blobio.service srv
+	  LEFT OUTER JOIN blobio.brr_blob_size sz ON (
+	  	sz.blob = srv.blob
+	  )
+  WHERE
+  	srv.discover_time >= now() + '-72 hours'
 ;
 ));
 my $pg_title = "$PGUSER\@$PGDATABASE\@$PGHOST:$PGPORT";
@@ -98,7 +155,28 @@ my $pg_title = "$PGUSER\@$PGDATABASE\@$PGHOST:$PGPORT";
 my (
 	$database_size_english,
 	$database_size_bytes,
+
+	$blob_count_sec,
+	$blob_size_sec,
+	$blob_size_english_sec,
+
+	$blob_count_min,
+	$blob_size_min,
+	$blob_size_english_min,
+
+	$blob_count_hr,
+	$blob_size_hr,
+	$blob_size_english_hr,
+
+	$blob_count_24hr,
+	$blob_size_24hr,
+	$blob_size_english_24hr,
+
+	$blob_count_72hr,
+	$blob_size_72hr,
+	$blob_size_english_72hr,
 ) = $q->fetchrow();
+
 unless ($database_size_english) {
 	print <<END;
    <span class="err">
@@ -111,12 +189,86 @@ END
 	return 1;
 }
 
+#  Note: reset for null sums: ought to be in sql case expression
+
+($blob_size_sec, $blob_size_english_sec) = ('0', '0') unless $blob_size_sec > 0;
+($blob_size_min, $blob_size_english_min) = ('0', '0') unless $blob_size_min > 0;
+($blob_size_hr, $blob_size_english_hr) = ('0', '0') unless $blob_size_hr > 0;
+($blob_size_24hr, $blob_size_english_24hr) = ('0', '0')
+	unless $blob_size_24hr > 0
+;
+($blob_size_72hr, $blob_size_english_72hr) = ('0', '0')
+	unless $blob_size_72hr > 0
+;
+
 print <<END;
    <h1>$pg_title</h1>
-   <h2>$database_size_english ($database_size_bytes bytes)</h2>
   </caption>
  </thead>
+ <tbody>
+  <tr>
+   <th>Statistic</th>
+   <th>Value</th>
+  </tr>
+
+  <tr>
+   <td>Database Size English</td>
+   <td>$database_size_english</td>
+  </tr>
+
+  <tr>
+   <td>Database Size Bytes</td>
+   <td>$database_size_bytes bytes</td>
+  </tr>
+
+  <tr>
+   <td>1 Second Blob Count</td>
+   <td>$blob_count_sec blobs</td>
+  </tr>
+  <tr>
+   <td>1 Second Blob Size</td>
+   <td>$blob_size_english_sec ($blob_size_sec bytes)</td>
+  </tr>
+
+  <tr>
+   <td>1 Minute Blob Count</td>
+   <td>$blob_count_min blobs</td>
+  </tr>
+  <tr>
+   <td>1 Minute Blob Size</td>
+   <td>$blob_size_english_min ($blob_size_min bytes)</td>
+  </tr>
+
+  <tr>
+   <td>1 Hour Blob Count</td>
+   <td>$blob_count_hr blobs</td>
+  </tr>
+  <tr>
+   <td>1 Hour Blob Size</td>
+   <td>$blob_size_english_hr ($blob_size_hr bytes)</td>
+  </tr>
+
+  <tr>
+   <td>24 Hour Blob Count</td>
+   <td>$blob_count_24hr blobs</td>
+  </tr>
+  <tr>
+   <td>24 Hour Blob Size</td>
+   <td>$blob_size_english_24hr ($blob_size_24hr bytes)</td>
+  </tr>
+
+  <tr>
+   <td>72 Hour Blob Count</td>
+   <td>$blob_count_72hr blobs</td>
+  </tr>
+  <tr>
+   <td>72 Hour Blob Size</td>
+   <td>$blob_size_english_72hr ($blob_size_72hr bytes)</td>
+  </tr>
+
+ </tbody>
 </table>
+
 END
 
 1;
