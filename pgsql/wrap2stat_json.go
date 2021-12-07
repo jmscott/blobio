646c58805c71f38659f45650482c7781c1276920
John the Scott <jmscott@setspace.com>
2021-12-06 18:36:23 -0600

    unstable version of new program wrap2stat_json

---
+++ pgsql/wrap2stat_json.go
@@ -0,0 +1,15 @@
+/*
+ *  Synopsis:
+ *	Summarize a blob request "wrap" set in json.
+ *  Usage:
+ *	wrap2stat_json <wrap-set-udig>
+ */
+
+package main;
+
+import "fmt";
+
+func main() {
+	
+	fmt.Printf("hello, world");
+}
