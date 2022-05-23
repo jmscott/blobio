9a232e953262038014ce96d92c47ad1b56ac2e7c
John the Scott <jmscott@setspace.com>
2022-05-23 15:37:28 -0500

    function: +uri_frisk_BLOBIO_SERVICE_query_args()

--- cli/blobio/qarg.c
+++ cli/blobio/qarg.c
@@ -5,15 +5,22 @@
 #include "blobio.h"
 
 /*
- *  Frisk query path of BLOBIO_SERVICE.
+ *  Frisk query args of uri BLOBIO_SERVICE.
+ *
+ *	tmo=20			#  timeout in seconds <= 255
+ *	brr=<path>		#  file path to write blob request record
+ *	tru=[fs|net=cidr]	#  who to trust (no digest check) during request
+ *
+ *  returning an error string or (char *) if passes.
  */
 char *
-uri_frisk_BLOBIO_SERVICE_query(char *query)
+uri_frisk_BLOBIO_SERVICE_query_args(char *query)
 {
 	char *q = query, c;
 
 	while ((c = *q++)) {
-		if (c == 't') {
+		switch (c) {
+		case 'b':		//  match "brr"
 			c = *q++;
 			if (!c)
 				return "unexpected null after 't' query arg";
@@ -24,9 +31,49 @@ uri_frisk_BLOBIO_SERVICE_query(char *query)
 				return "unexpected null after 'tm' query arg";
 			if (c != 'o')
 				return "unexpected char after 'tm' query arg";
-			if (*q && *q != '&')
-				return "no & char after tmp query arg";
+			break;
+		case 't':		//  match "tmo"
+			c = *q++;
+			if (!c)
+				return "unexpected null after 't' query arg";
+			switch (c) {
+			case 'm':
+				c = *q++;
+				if (!c)
+					return "unexpected null after 'tm'"
+					       "query arg";
+				if (c != 'o')
+					return "unexpected char after 'tm'"
+					       "query arg";
+				break;
+			case 'r':
+				c = *q++;
+				if (!c)
+					return "unexpected null after 'tr' "
+					       "query arg";
+				if (c != 'u')
+					return "unexpected char after 'tr' "
+					       "query arg";
+				break;
+			default:
+				return "unexpected char after 't' query arg";
+			}
+			break;
 		}
+		if (*q && *q != '&')
+			return "did not see expected & character";
 	}
-	return "unknown query arg";
+	return (char *)0;
+}
+
+/*
+ *  Extract a timeout value from a frisked query arg string.
+ */
+char *
+extract_query_arg_tmo(char **p_query, unsigned int tmo)
+{
+	(void)p_query;
+	(void)tmo;
+
+	return (char *)0;
 }
