c013f553fe660c91ef546c2ff22f6010568fa247
John the Scott <jmscott@setspace.com>
2022-10-24 18:46:34 -0500

    unstable checkpoint

--- cli/bio-frisk-brr.c
+++ cli/bio-frisk-brr.c
@@ -1,246 +1,524 @@
 /*
  *  Synopsis:
- *  	Frisk a set off blob request records read from stdin.
- *  Usage:
- *  	cat $BLOBIO_ROOT/spool/fs.brr | bio-frisk-brr
- *  Exit Status:
- *	0	ok, no erros
- *	1	error in blob request records, offend line written on stderr
+ *	Is a stream of data a well formed brr log file: yes or no or empty?
+ *  Description:
+ *  	A blob request record log file consists of lines of ascii text
+ *  	with 7 tab separated fields matching
+ *
+ *		start time: YYYY-MM-DDTHH:MM:SS.NS9(([-+]hh:mm)|Z)
+ *		\t
+ *		transport: matches perl5 re:
+ *
+ *			[a-z][a-z0-9]{,7}~[[:graph:]]{1,128})
+ *
+ *		\t
+ *		verb: get|put|give|take|eat|wrap|roll
+ *		\t
+ *		udig: [a-z][a-z0-9]{7}:hash-digest{1,128}
+ *		\t
+ *		chat history:
+ *			ok|no			all verbs
+ *
+ *			ok,ok|ok,no		when verb is "give" or "take"
+ *
+ *			ok,ok,ok|ok,ok,no	only "take"
+ *		\t
+ *		byte count: 0 <= 2^63 - 1
+ *		\t
+ *		wall duration: sec.ns where
+ *			       0<=sec&&sec <= 2^31 -1 && 0<=ns&&ns <=999999999
+ *  Exit:
+ *  	0	-> is a brr log
+ *  	1	-> is not a brr log
+ *	2	-> is empty
+ *  	3	-> unexpected error
+ *  Blame:
+ *  	jmscott@setspace.com
+ *  Note:
+ *	Would be nice to remove dependency upon stdio library.
  */
+
+#include <unistd.h>
+#include <string.h>
 #include <stdio.h>
-#include "jmscott/libjmscott.h"
+#include <stdlib.h>
+#include <errno.h>
+#include <ctype.h>
+#include <limits.h>
 
-char	jmscott_progname[] = "bio-frisk-brr";
-
-#define MIN_BRR		95
-#define MAX_BRR		371
+static char *jmscott_program =	"bio-frisk-brr";
+static char digits[] = "0123456789";
+static char lower_alnum[] = "abcdefghijklmnopqrstuvwxyz0123456789";
 
 /*
- *  Build the brr record from a command line argument.
- *  Update the pointer to the position in the buffer.
+ *  ASCII graphical characters allowed in brr transport and udig.
+ */
+static char graph_set[] =
+		"!\"#$%&'()*+,-./"	//  0x32 -> 0x47
+		"0123456789:;<=>?"	//  0x48 -> 0x63
+		"@ABCDEFGHIJKLMNO"	//  0x64 -> 0x79
+		"PQRSTUVWXYZ[\\]^_"	//  0x50 -> 0x5f
+		"`abcdefghijklmno"	//  0x60 -> 0x6f
+		"pqrstuvwxyz{|}~"	//  0x70 -> 0x7e
+;
+
+static void
+die(char *msg)
+{
+	jmscott_die(3, msg);
+}
+
+/*
+ *  Verify that exactly the first 'n' characters in a string are in a 
+ *  set.  If a char is not in the set, then exit this process falsely;
+ *  otherwise, reset the source pointer to src + n;
+ */
+static void 
+in_set(char **src, int n, char *set)
+{
+	char *s, *s_end;
+
+	s = *src;
+	s_end = s + n;
+	while (s < s_end) {
+		char c = *s++;
+
+		if (c == 0 || strchr(set, c) == NULL)
+			exit(1);
+	}
+	*src = s_end;
+}
+/*
+ *  Verify that a range of characters in a string are in a 
+ *  set.  If a char is not in the set, then exit this process falsely;
+ *  otherwise, reset the source pointer to first char not in the set.
+ */
+static void 
+in_range(char **src, int upper, int lower, char *set)
+{
+	char *s, *s_end;
+
+	s = *src;
+	s_end = s + upper;
+	while (s < s_end) {
+		char c = *s++;
+
+		if (strchr(set, c) != NULL)
+			continue;
+		if (s - *src <= lower)	//  too few characters
+			exit(1);
+		--s;
+		break;
+	}
+	*src = s;
+}
+
+/*
+ *  Scan up to 'limit' chars that are in set
+ *  and set src to first char not in the set.
+ *  Null character in the 'src' fails.
+ */
+static void 
+scan_set(char **src, int limit, char *set)
+{
+	char *s, *s_end;
+
+	s = *src;
+	s_end = s + limit;
+	while (s < s_end) {
+		char c = *s;
+
+		if (c == 0)
+			exit(1);
+		if (strchr(set, c) == NULL)
+			break;
+		s++;
+	}
+	*src = s;
+}
+
+/*
+ *  Match RFC3339Nano time stamp:
  *
- *  Note:
- *	Need to be more strict about correctness of brr fields.
- *	For example, a char set per arg would be easy to add.
+ *	2010-12-03T04:47:05.123456789+00:01
+ *	2010-12-03T04:47:05.123456789-00:03
+ *	2010-12-03T04:47:05.123456789Z
  */
 static void
-arg2brr(char *name, char *arg, size_t min_len, int size, char **brr)
+scan_rfc399nano(char **src)
 {
-	char *tgt, *src;
-	int n;
+	char *p = *src;
 
-	if (strlen(arg) < min_len)
-		die3(EXIT_BAD_BRR, "argument too short", name, arg);
-	n = 0;
-	src = arg;
-	tgt = *brr;
-	while (*src && n++ < size)
-		*tgt++ = *src++;
-	if (n >= size && *src)
-		die3(EXIT_BAD_BRR, "arg too big for brr field", name, arg); 
-	*tgt++ = '\t';
-	*brr = tgt;
-}
+	/*
+	 *  Year:
+	 *	match [2345]YYY
+	 */
+	if ('2' > *p || *p > '5')
+		exit(1);
+	p++;
+	in_set(&p, 3, digits);
 
-static void
-in_char_set(char *name, char *arg, char *set)
-{
-	char c;
-	char *a = arg;
+	/*
+	 *  Month:
+	 *	match -[01][0-9]
+	 */
+	if (*p++ != '-')
+		exit(1);
+	if (*p != '0' && *p != '1')
+		exit(1);
+	p++;
+	if ('0' > *p || *p > '9')
+		exit(1);
+	p++;
 
-	//  verify all chars exist in the set
-	//  Note:  can replace with a table lookup.
+	/*
+	 *  Day:
+	 *	match -[0123][0-9]
+	 */
+	if (*p++ != '-')
+		exit(1);
+	if ('0' > *p || *p > '3')
+		exit(1);
+	p++;
+	if ('0' > *p || *p > '9')
+		exit(1);
+	p++;
 
-	while ((c = *a++)) {
-		char *sp = set;
-		while (*sp) {
-			if (c == *sp)
-				break;
-			sp++;
-		}
-		if (*sp == 0)
-			die3(EXIT_BAD_BRR, name, "illegal char exists", arg);
+	/*
+	 *  Hour:
+	 *	match T[012][0-9]
+	 */
+	if (*p++ != 'T')
+		exit(1);
+	if ('0' > *p || *p > '2')
+		exit(1);
+	p++;
+	if ('0' > *p || *p > '9')
+		exit(1);
+	p++;
+
+	/*
+	 *  Minute:
+	 *	match :[0-5][0-9]
+	 */
+	if (*p++ != ':')
+		exit(1);
+	if ('0' > *p || *p > '5')
+		exit(1);
+	p++;
+	if ('0' > *p || *p > '9')
+		exit(1);
+	p++;
+
+	/*
+	 *  Second:
+	 *	match :[0-5][0-9]
+	 */
+	if (*p++ != ':')
+		exit(1);
+	if ('0' > *p || *p > '5')
+		exit(1);
+	p++;
+	if ('0' > *p || *p > '9')
+		exit(1);
+	p++;
+	if (*p++ != '.')
+		exit(1);
+	in_range(&p, 9, 1, digits);
+
+	switch (*p++) {
+	case 'Z':
+		break;
+	case '+': case '-':
+		in_set(&p, 2, digits);
+		if (*p++ != ':')
+			exit(1);
+		in_set(&p, 2, digits);
+		break;
+	default:
+		exit(1);
 	}
+	if (*p++ != '\t')
+		exit(1);
+	*src = p;
 }
 
+/*
+ *  Scan the transport field which matches this perl5 regex:
+ *
+ *	[a-z][a-z0-9]{0,7}~([[:graph:]{1,128})
+ */
 static void
-is_start_time(char *arg, char **brr)
+scan_transport(char **src)
 {
-	static char start_time_set[16] = {
-		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-		  '-', 'T', ':', '.', '+', 0
-	};
-	char *nm = "start time";
+	char c, *p = *src;
 
-	arg2brr(nm, arg, 26,  10+1+8+1+9+1+6, brr);
-	if (arg[4] != '-')
-		die3(EXIT_BAD_BRR, nm, "dash not at pos 5", arg);
-	if (arg[7] != '-')
-		die3(EXIT_BAD_BRR, nm, "dash not at pos 7", arg);
-	if (arg[10] != 'T')
-		die3(EXIT_BAD_BRR, nm, "T not at pos 11", arg);
-	if (arg[13] != ':')
-		die3(EXIT_BAD_BRR, nm, "colon not at pos 14", arg);
-	if (arg[16] != ':')
-		die3(EXIT_BAD_BRR, nm, "colon not at pos 17", arg);
-	if (arg[19] != '.')
-		die3(EXIT_BAD_BRR, nm, "colon not at pos 20", arg);
-	in_char_set(nm, arg, start_time_set);
+	/*
+	 *  Scan the network flow field which matches this perl5 regex:
+	 *
+	 *	[a-z][a-z0-9]{0,7}([[:graph:]{1,128})
+	 */
+	c = *p++;
+	if ('a' > c || c > 'z')
+		exit(1);
+
+	scan_set(&p, 7, lower_alnum);
+	if (*p++ != '~')
+		exit(1);
+	scan_set(&p, 128, graph_set);
+	if (*p++ != '\t')
+		exit(1);
+	*src = p;
 }
 
-static void
-is_transport(char *arg, char **brr)
+/*
+ *  Blobio verb: get|put|give|take|eat|what.
+ */
+static char *
+scan_verb(char **src)
 {
-	char *nm = "transport";
-	char c, *a;
+	char c, *p = *src, *verb;
 
-	arg2brr(nm, arg, 3, 8+1+128, brr);
-	a = arg;
-	if (*a < 'a' || *a > 'z')
-		die3(EXIT_BAD_BRR, nm, "protocol not ^[a-z]", arg);
-	a++;
+	c = *p++;
+	if (c == 'g') {			/* get|give */
+		c = *p++;
+		if (c == 'e') {			/* get */
+			if (*p++ != 't')
+				exit(1);	/* give */
+			verb = "get";
 
-	//  check chars in protocol
+		} else if (c == 'i') {
+			if (*p++ != 'v' || *p++ != 'e')
+				exit(1);
+			verb = "give";
+		} else
+			exit(1);
+	} else if (c == 'p') {		/* put */
+		if (*p++ != 'u' || *p++ != 't')
+			exit(1);
+		verb = "put";
+	} else if (c == 't') {		/* take */
+		if (*p++ != 'a' || *p++ != 'k' || *p++ != 'e')
+			exit(1);
+		verb = "take";
+	} else if (c == 'e') {		/* eat */
+		if (*p++ != 'a' || *p++ != 't')
+			exit(1);
+		verb = "eat";
+	} else if (c == 'w') {		/* wrap */
+		if (*p++ != 'r' || *p++ != 'a' || *p++ != 'p')
+			exit(1);
+		verb = "wrap";
+	} else if (c == 'r') {		/* roll */
+		if (*p++ != 'o' || *p++ != 'l' || *p++ != 'l')
+			exit(1);
+		verb = "roll";
+	} else
+		exit(1);
+	if (*p++ != '\t')
+		exit(1);
+	*src = p;
+	return verb;
+}
+/*
+ *  Scan the uniform digest that looks like:
+ *
+ *  	algorithm:hash-digest
+ */
+static void
+scan_udig(char **src)
+{
+	char *p = *src;
 
-	while ((c = *a) && c != '~') {
-		if (a - arg > 7)
-			die3(EXIT_BAD_BRR, nm, "protocol > 8 characters", arg);
-		if (!isdigit(c) && !islower(c))
-			die3(EXIT_BAD_BRR, nm, "protocol not [a-z0-9]", arg);
-		a++;
+	/*
+	 *  First char of algorithm must be lower case alpha.
+	 */
+	if ('a' > *p || *p > 'z')
+		exit(1);
+	p++;
+	/*
+	 *  Up to 7 alphnum chars before colon.
+	 */
+	scan_set(&p, 7, lower_alnum);
+
+	/*
+	 *  Expect colon between algorithm and digest.
+	 */
+	if (*p++ != ':')
+		exit(1);	/* first char in algo not : */
+
+	scan_set(&p, 128, graph_set);
+
+	if (*p++ != '\t')
+		exit(1);
+	*src = p;
+}
+
+/*
+ *  Chat History:
+ *	ok		->	verb in {get,put,eat,wrap,roll}
+ *	ok,ok		->	verb in {give}
+ *	ok,ok,ok	->	verb in {take}
+ *	no		->	verb in	{
+ *					get,put,eat,
+ *					give,take,
+ *					wrap,roll
+ *				}
+ *	ok,no		->	verb in {give,take}
+ *	ok,ok,no	->	verb in {take}
+ *
+ *  All chat histories start with character 'o' or 'n'.
+ *  Scan the chat history, verifying the reposnse
+ *  is correct for the verb.
+ */
+static void
+scan_chat_history(char *verb, char **src)
+{
+	char *p, *q, ch[9], v1, v2;
+	int len;
+
+	p = *src;
+	q = p;
+
+	scan_set(&q, 8, "nok,");
+	len = q - p;
+	if (len < 2)
+		exit(1);
+	memcpy(ch, p, len);
+	p = q;
+	ch[len] = 0;
+	v1 = verb[0];
+	v2 = verb[1];
+
+	/*
+	 *  Only verbs: get/put/eat/wrap/roll ca
+	 *  Remember, the chat history is the observed value during the
+	 *  network flow from the server's point of view.
+	 */
+	if (strcmp("no", ch) == 0 || strcmp("ok", ch) == 0)
+		goto done;
+
+	/*
+	 *  Both "give" and "take" can chat either "ok,ok" or "ok,no"
+	 */
+	if (strcmp("ok,ok", ch) == 0 || strcmp("ok,no", ch) == 0) {
+		if ((v1 == 'g' && v2 == 'i') || v1 == 't')
+			goto done;
+		exit(1);
 	}
-	if (c == 0)
-		die3(EXIT_BAD_BRR, nm, "tilda not seen", arg);
-	a++;
-	if (strlen(a) > 128)
-		die3(EXIT_BAD_BRR, nm, "flow: length > 128", arg);
-
-	while ((c = *a++))
-		if (!isgraph(c) || !isascii(c))
-			die3(EXIT_BAD_BRR, nm, "flow: bad char", arg);
+	/*
+	 *  Only a "take" can do "ok,ok,ok" or "ok,ok,no".
+	 */
+	if ((strcmp("ok,ok,ok",ch)==0 || strcmp("ok,ok,no", ch)==0) && v1=='t')
+		goto done;
+	exit(1);
+done:
+	if (*p++ != '\t')
+		exit(1);
+	*src = p;
 }
 
+/*
+ *  Scan for an unsigned int <= 2^63 -1.
+ *
+ *  Note:
+ *  	Need a compile time pragma to insure the
+ *
+ *  		sizeof long long int == 8
+ *
+ *  	is true.
+ */
 static void
-is_udig(char *arg, char **brr)
+scan_uint63(char **src, char c_end)
 {
-	char *nm = "udig";
-	char c, *a;
-	size_t len;
+	char *p, *q;
+	long long v;
 
-	arg2brr(nm, arg, 1 + 1 + 28, 8+1+128, brr);
-	a = arg;
-	if (*a < 'a' || *a > 'z')
-		die3(EXIT_BAD_BRR, nm, "algorithm not ^[a-z]", arg);
-	a++;
+	p = *src;
+	q = p;
 
-	//  check digits in algorithm
+	scan_set(&p, 21, digits);
 
-	while ((c = *a) && c != ':') {
-		if (a - arg > 7)
-			die3(EXIT_BAD_BRR, nm, "algorithm > 8 characters", arg);
-		if (!isdigit(c) && !islower(c))
-			die3(EXIT_BAD_BRR, nm, "algorithm not [a-z0-9]", arg);
-		a++;
-	}
-	if (c == 0)
-		die3(EXIT_BAD_BRR, nm, "no colon at end of algorithm", arg);
+	errno = 0;
+	v = strtoll(q, (char **)0, 10);
 
-	a++;
-	len = strlen(a);
-	if (len < 32)
-		die3(EXIT_BAD_BRR, nm, "digest: length < 32", arg);
-	if (len > 128)
-		die3(EXIT_BAD_BRR, nm, "digest: length > 128", arg);
-
-	while ((c = *a++))
-		if (!isgraph(c) || !isascii(c))
-			die3(EXIT_BAD_BRR, nm, "digest: bad char", arg);
+	if ((v == LLONG_MAX && errno == ERANGE) ||
+	    (LLONG_MAX > 9223372036854775807 &&
+	     v > 9223372036854775807))
+		exit(1);
+	if (*p++ != c_end)
+		exit(1);
+	*src = p;
 }
 
+/*
+ *  Duration: seconds.nsecs, where seconds ~ /^\{1,9}$/
+ *  	      and nsecs ~ /^\d{9}$/
+ *  	      and seconds <= 2147483647
+ */
 static void
-is_blob_size(char *arg, char **brr)
+scan_duration(char **src, char c_end)
 {
-	static char digit_set[11] = {
-		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-		0
-	};
+	char *p, *q;
+	long v;
 
-	arg2brr("blob size", arg, 1, 19, brr);
-	in_char_set("blob size", arg, digit_set);
-}
+	p = *src;
+	q = p;
 
-static void
-is_wall_duration(char *arg, char **brr)
-{
-	static char duration_set[12] = {
-		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-		'.', 0
-	};
-	arg2brr("wall duration", arg, 1, 10+1+9, brr);
-	in_char_set("wall duration", arg, duration_set);
+	scan_set(&p, 10, digits);
+
+	errno = 0;
+	v = strtoll(q, (char **)0, 10);
+
+	if ((v == LONG_MAX && errno == ERANGE) ||
+	    (LONG_MAX > 2147483647 && v > 2147483647))
+		exit(1);
+	if (*p++ != '.')
+		exit(1);
+	in_range(&p, 9, 1, digits);
+	if (*p++ != c_end)
+		exit(1);
+	*src = p;
 }
 
 int
-main(int argc, char **argv)
+main()
 {
-	// room for brr + newline + null
-	char brr[MAX_BRR + 1 + 1], *b;
-	char *path;
-	int fd;
-
-	if (argc != 9)
-		die(EXIT_BAD_ARGC, "wrong number arguments");
-	path = argv[1];
+	char buf[10 * 1024], *p;
+	char *verb;
+	int read_input = 0;
 
 	/*
-	 *  Build the blob request record from command line arguments
+	 *  Note:
+	 *	Please replace with read().
 	 */
-	b = brr;
+	while (fgets(buf, sizeof buf, stdin)) {
+		read_input = 1;
 
-	is_start_time(argv[2], &b);
-	is_transport(argv[3], &b);
+		p = buf;
 
-	//  verb
+		//  start time of request
+		scan_rfc399nano(&p);
+		scan_transport(&p);
+		verb = scan_verb(&p);
 
-	arg2brr("verb", argv[4], 3, 8, &b);
-	if (strcmp("get", argv[4])					&&
-	    strcmp("put", argv[4])					&&
-	    strcmp("give", argv[4])					&&
-	    strcmp("take", argv[4])					&&
-	    strcmp("eat", argv[4])					&&
-	    strcmp("wrap", argv[4])					&&
-	    strcmp("roll", argv[4]))
-		die2(EXIT_BAD_BRR, "not verb", argv[4]);
+		scan_udig(&p);
+		scan_chat_history(verb, &p);
 
-	is_udig(argv[5], &b);
+		//  byte count
+		scan_uint63(&p, '\t');
 
-	//  chat history
+		//  wall duration
+		scan_duration(&p, '\n');
 
-	arg2brr("chat history", argv[6], 2, 8, &b);
-	if (strcmp("ok", argv[6])					&&
-	    strcmp("no", argv[6])					&&
-	    strcmp("ok,ok", argv[6])					&&
-	    strcmp("ok,no", argv[6])					&&
-	    strcmp("ok,ok,ok", argv[6])					&&
-	    strcmp("ok,ok,no", argv[6]))
-		die2(EXIT_BAD_BRR, "not chat history", argv[6]);
+	}
+	if (ferror(stdin))
+		die("ferror(stdin) returned true");
+	if (read_input)
+		exit(feof(stdin) ? 0 : 1);
 
-	is_blob_size(argv[7], &b);
-
-	is_wall_duration(argv[8], &b);
-
-	b[-1] = '\n';		//  zap trailing tab
-
-	fd = _open(path,
-		   O_WRONLY | O_APPEND | O_CREAT,
-		   S_IRUSR | S_IWUSR | S_IRGRP
-	);
-
-	if (b - brr < MIN_BRR)
-		die(EXIT_BAD_BRR, "brr < 95 bytes");
-
-	// atomically write exactly the number bytes in the blob request record
-	_write(fd, brr, b - brr);
-	_close(fd);
-	_exit(0);
+	/*
+	 *  Empty set.
+	 */
+	exit(2);
 }
