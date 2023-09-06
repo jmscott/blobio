fcdc9765710d6d1e71e1cbabd519a47bab7638ae
John the Scott <jmscott@setspace.com>
2023-09-06 05:00:52 -0500

    zapped service cache4

--- cli/blobio/service.c
+++ cli/blobio/service.c
@@ -5,13 +5,11 @@
 #include "blobio.h"
 
 extern struct service	bio4_service;
-extern struct service	cache4_service;
 extern struct service	fs_service;
 
 struct service *services[] =
 {
 	&bio4_service,
-	&cache4_service,
 	&fs_service,
 	(struct service *)0
 };
