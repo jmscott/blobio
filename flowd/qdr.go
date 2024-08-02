e975e72ff21a5e3a3f96876b4eb245a240967a19
John the Scott <jmscott@setspace.com>
2024-08-02 00:29:14 -0500

    zapped uneeded qdr_field

--- flowd/qdr.go
+++ flowd/qdr.go
@@ -7,21 +7,6 @@ import (
 	. "time"
 )
 
-type qdr_field uint8
-
-const (
-	//  field offsets into query detail record (qdr)
-	qdr_START_TIME = iota
-	qdr_FLOW_SEQUENCE
-	qdr_QUERY_NAME
-	qdr_TERMINATION_CLASS
-	qdr_UDIG
-	qdr_SQLSTATE
-	qdr_ROWS_AFFECTED
-	qdr_WALL_DURATION
-	qdr_QUERY_DURATION
-)
-
 type qdr struct {
 	start_time        Time
 	flow_sequence     int64
@@ -32,32 +17,6 @@ type qdr struct {
 	rows_affected     int64
 	wall_duration     Duration
 	query_duration    Duration
-}
-
-func (qf qdr_field) String() string {
-
-	switch qf {
-	case qdr_START_TIME:
-		return "start_time"
-	case qdr_FLOW_SEQUENCE:
-		return "flow_sequence"
-	case qdr_QUERY_NAME:
-		return "query_name"
-	case qdr_TERMINATION_CLASS:
-		return "termination_class"
-	case qdr_UDIG:
-		return "udig"
-	case qdr_SQLSTATE:
-		return "sqlstate"
-	case qdr_ROWS_AFFECTED:
-		return "rows_affected"
-	case qdr_WALL_DURATION:
-		return "wall_duration"
-	case qdr_QUERY_DURATION:
-		return "query_duration"
-	default:
-		panic("impossible qdr field value")
-	}
 }
 
 //  Note: needs to be YYYY-MM-DD!
