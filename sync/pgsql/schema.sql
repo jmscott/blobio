ae69e963ef1f4a7fbbadc4bf7d63f357c6c368ea
John the Scott <jmscott@setspace.com>
2025-03-26 16:31:16 -0500

    function interval_terse_english()

--- sync/pgsql/schema.sql
+++ sync/pgsql/schema.sql
@@ -290,7 +290,7 @@ DROP VIEW IF EXISTS service CASCADE;
 CREATE VIEW service AS
   SELECT
 	blob,
-	start_time
+	start_time AS discover_time
     FROM
     	brr_discover
 ;
@@ -319,5 +319,132 @@ COMMENT ON view rummy IS
   'Blobs with known unknown attributes'
 ;
 ALTER VIEW rummy OWNER TO :DBOWNER;
+
+DROP FUNCTION IF EXISTS interval_terse_english(interval) CASCADE;
+CREATE OR REPLACE FUNCTION interval_terse_english(duration interval)
+  RETURNS text AS $$
+  DECLARE
+  	elapsed numeric;
+	english text;
+
+  	yr numeric;
+  	yr_unit text;
+
+  	mon numeric;
+  	mon_unit text;
+
+  	day numeric;
+  	day_unit text;
+
+  	hr numeric;
+  	hr_unit text;
+
+  	min numeric;
+  	min_unit text;
+
+	sec numeric;
+	sec_unit text;
+
+	sec_per_yr numeric;
+	sec_per_mon numeric;
+	sec_per_day numeric;
+	sec_per_hr numeric;
+	sec_per_min numeric;
+  BEGIN
+  	elapsed = round(extract(epoch from duration));
+
+	sec_per_yr = extract(epoch from '1 year'::interval);
+	sec_per_mon = extract(epoch from '1 mon'::interval);
+	sec_per_day = extract(epoch from '1 day'::interval);
+	sec_per_hr = extract(epoch from '1 hour'::interval);
+	sec_per_min = extract(epoch from '1 minute'::interval);
+
+	yr = 0;
+	yr_unit = ' ';
+
+	mon = 0;
+	mon_unit = ' ';
+
+	day = 0;
+	day_unit = ' ';
+
+	hr = 0;
+	hr_unit = ' ';
+
+	min = 0;
+	min_unit = ' ';
+
+	sec = 0;
+	sec_unit = ' ';
+
+	-- years
+
+	if elapsed >= sec_per_yr then
+		yr = round(elapsed / sec_per_yr);
+		yr_unit = 'yr ';
+
+		elapsed = elapsed - (yr * sec_per_yr);
+	end if;
+
+	--  months
+
+	if elapsed >= sec_per_mon then
+		mon = round(elapsed / sec_per_mon);
+		mon_unit = 'mon ';
+
+		elapsed = elapsed - (mon * sec_per_mon);
+	end if;
+
+	--  days
+
+	if elapsed >= sec_per_day then
+		day = round(elapsed / sec_per_day);
+		day_unit = 'day ';
+
+		elapsed = elapsed - (day * sec_per_day);
+	end if;
+
+	--  hours
+	if elapsed >= sec_per_hr then
+		hr = round(elapsed / sec_per_hr);
+		hr_unit = 'hr ';
+
+		elapsed = elapsed - (hr * sec_per_hr);
+	end if;
+
+	--  minutes
+	if elapsed >= sec_per_min then
+		min = round(elapsed / sec_per_min);
+		min_unit = 'min ';
+
+		elapsed = elapsed - (min * sec_per_min);
+	end if;
+
+	--  seconds
+	if elapsed > 0 then
+		sec = round(elapsed / 60);
+		sec_unit = 'sec ';
+	end if;
+
+	english =
+		yr || yr_unit ||
+		mon || mon_unit ||
+		day || day_unit ||
+		hr || hr_unit ||
+		min || min_unit ||
+		sec || sec_unit
+	;
+	english = regexp_replace(english, ' *0 *', '', 'g');
+	if english = '' then
+		english = '0sec';
+	end if;
+
+	return english;
+
+  END $$ LANGUAGE plpgsql VOLATILE STRICT
+;
+COMMENT ON FUNCTION interval_terse_english(interval) IS
+  'Convert time interval to full precision, terse english, e.g. 3hr12min5sec'
+;
 
 COMMIT TRANSACTION;
